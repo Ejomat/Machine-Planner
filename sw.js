022993d707c1b919c936e262e972a576eb69c37b
Ejomat <ygoktasejot@gmail.com>
2025-08-01 17:32:42 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'makine-bulucu-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache açıldı');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
